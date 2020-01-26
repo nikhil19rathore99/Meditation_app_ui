ee2cdf28f1f46a9d72b9208bc2e782362244d725
nikhil19rathore99 <nikhilrathoreb4i@gmail.com>
2020-01-26 18:55:54 +0530

    Flutter Meditation_APP_UI

---
+++ android/app/src/main/kotlin/com/example/prac_day_02_meditation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prac_day_02_meditation
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
